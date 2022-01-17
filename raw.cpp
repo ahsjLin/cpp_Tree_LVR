3e36687cd4ee86d4d940bb931ae040f7ff3c98b4
sj <sjlin8162018@gmail.com>
2022-01-17 18:37:00 +0800

    2022-01-17_18-36

--- raw.cpp
+++ raw.cpp
@@ -18,9 +18,11 @@ int n_node_now=0;
 
 struct BSTNode * newBSTNode(int newNode){
 	struct BSTNode * rootNode = new struct BSTNode;
-	String str = "Create new node and place (" + IntToStr(newNode) +")";
-	Form1->Memo2->Lines->Add(str);
-	Form1->Memo2->Lines->Add("");
+	if(Form1->CheckBox1->Checked){
+		String str = "Create new node and place (" + IntToStr(newNode) +")";
+		Form1->Memo2->Lines->Add(str);
+		Form1->Memo2->Lines->Add("");
+	}
 	if(StrToInt(Form1->Edit1->Text)==++n_node_now) {
 		insert_order += IntToStr(newNode);
 		n_node_now=0;
@@ -43,13 +45,17 @@ void printBST(struct BSTNode *node){
 struct BSTNode * insertBST(struct BSTNode* node, int newNode){
 	if(node==NULL) return newBSTNode(newNode);
 	if(node->data>=newNode){
-		String str = "Finding leaf (" + IntToStr(newNode) + ") -> go left";
-		Form1->Memo2->Lines->Add(str);
+		if(Form1->CheckBox1->Checked){
+			String str = "Finding leaf (" + IntToStr(newNode) + ") -> go left";
+			Form1->Memo2->Lines->Add(str);
+		}
 		node->leftchild = insertBST(node->leftchild, newNode);
 	}
 	else{
-		String str = "Finding leaf (" + IntToStr(newNode) + ") -> go right";
-		Form1->Memo2->Lines->Add(str);
+		if(Form1->CheckBox1->Checked){
+			String str = "Finding leaf (" + IntToStr(newNode) + ") -> go right";
+			Form1->Memo2->Lines->Add(str);
+		}
 		node->rightchild = insertBST(node->rightchild, newNode);
 	}
 	return node;
@@ -58,10 +64,12 @@ void deleteBST(struct BSTNode* root){
 	if(root!=NULL){
 		deleteBST(root->leftchild);
 		deleteBST(root->rightchild);
-		String str = "Delete node(" + IntToStr(root->data)+")";
-		Form1->Memo3->Lines->Add(str);
+		if(Form1->CheckBox1->Checked){
+			String str = "Delete node(" + IntToStr(root->data)+")";
+			Form1->Memo3->Lines->Add(str);
+		}
 		delete root;
-		Form1->Memo3->Lines->Add("");
+		//Form1->Memo3->Lines->Add("");
 	}
 
 }
@@ -89,26 +97,46 @@ void __fastcall TForm1::Button1Click(TObject *Sender)
 	}
 	if(n_node>1000)
 	{
-		ShowMessage("Less than 1000.");
-		return;
+		ShowMessage("Less than 1000. Closed show processing");
+		CheckBox1->Checked = false;
 	}
+	// spanning tree time
+	clock_t t1 = clock();
 	srand(time(NULL));
 	for(int i=0; i<n_node; i++){
 		newNode = rand()%10000;
 		root = insertBST(root, newNode);
 	}
+	clock_t t2 = clock();
+	double d = ((double)(t2-t1))/CLOCKS_PER_SEC;
 
 	tree = "";
+
 	printBST(root);
+
+	String str = "Spanning time: " + FloatToStr(d) +"s";
+
 	Memo1->Lines->Add("The order of inserted value:");
 	Memo1->Lines->Add(insert_order);
     insert_order = "";
 	Memo1->Lines->Add("");
 	Memo1->Lines->Add("The LVR way to show data of nodes in tree:");
 	Memo1->Lines->Add(tree);
-    Memo3->Lines->Add("LRV way to Delete nodes.");
+
+	Memo2->Lines->Add("");
+	Memo2->Lines->Add(str);
+
+
+	Memo3->Lines->Add("The LRV way to Delete nodes.");
+	clock_t t3 = clock();
 	deleteBST(root);
+	clock_t t4 = clock();
+	d = ((double)(t4-t3))/CLOCKS_PER_SEC;
+	str = "Detele time: " + FloatToStr(d) +"s";
+	Memo3->Lines->Add("");
+	Memo3->Lines->Add(str);
 	root = NULL;
+	//ShowMessage("t1" + FloatToStr(t1)+"\nt1" + FloatToStr(t2)+"\nd:" + FloatToStr(d));
 }
 //---------------------------------------------------------------------------
 void __fastcall TForm1::TrackBar1Change(TObject *Sender)
